193f4146c9f7ecaf3ba0dfa89abde32d5c7211cc
WisleyASousa <wisleya.sousa@outlook.com.br>
2022-12-02 12:48:22 -0300

    criando secao perfil e cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
-import Head from 'next/head'
-import {BsFillMoonStarsFill} from 'react-icons/bs'
+import Head from 'next/head';
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai';
+import Image from 'next/image';
+import deved from '../public/IMG_5426.png';
+import design from '../public/design.png';
+import code from '../public/code.png';
+import consulting from '../public/consulting.png';
+
 
 export default function Home() {
   return (
@@ -14,14 +21,78 @@ export default function Home() {
         <section
           className="min-h-screen ">
             <nav className='py-10 mb-12 flex justify-between'>
-              <h1 className='text-lg'>Wisley A. Sousa</h1>
+              <h1 className='text-xl hover:drop-shadow-xl hover:backdrop-blur-sm hover:cursor-pointer italic font-burtons font-semibold'>Portfolio</h1>
               <ul className='flex items-center'>
                 <li>
                   <BsFillMoonStarsFill className='cursor-pointer text-xl'/>
                 </li>
-                <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a></li>
+                <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 font-semibold' href='#'>Resume</a></li>
               </ul>
             </nav>
+            <div className='text-center p-10'>
+              <h2 className='text-4xl py-2 text-teal-700 font-medium'>Wisley A. Sousa</h2>
+              <h3 className='text-2xl py-2 '>Front-end Developer</h3>
+              <p className='text-md py-5 leading-6 text-gray-800 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+            </div>
+            <div className='text-4xl flex justify-center gap-12 py-2 text-gray-600'>
+              <AiFillGithub />
+              <AiFillLinkedin />
+            </div>
+            <div className='relative bg-gradient-to-b from-teal-500 rounded-full w-40 mt-10 mx-auto'>
+              <Image src={deved} alt='developer' />
+            </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className='text-2xl py-1'>Services I offer</h3>
+            <p className='text-md py-2 leading-6 text-gray-800'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.Lorem ipsum <span className='text-teal-500'>dolor </span> sit amet consectetur adipisicing elit. Quisquam, quae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.
+            </p>
+            <p className='text-md py-2 leading-6 text-gray-800'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum <span className='text-teal-500'>dolor </span> sit amet consectetur adipisicing elit. Quisquam, quae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.
+            </p>
+          </div>
+          <div>
+            <div className='text-center bg-gradient-to-r from-cyan-100 to-teal-200 shadow-lg p-10 rounded-xl my-10'>
+              <Image src={design} width={100} height={100} alt={''} className='mx-auto' />
+              <h3 className='text-lg font-medium pt-6 pb-2'>Beautiful Designs</h3>
+              <p className='py-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.
+              </p>
+              <h4 className='py-4 text-teal-900'>Desgin tools I use</h4>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p className='text-gray-800 py-1'>Illustrator</p>
+              <p className='text-gray-800 py-1'>Figma</p>
+            </div>
+            <div className='text-center bg-gradient-to-r from-cyan-100 to-teal-200 shadow-lg p-10 rounded-xl my-10'>
+              <Image src={consulting} width={100} height={100} alt={''} className='mx-auto' />
+              <h3 className='text-lg font-medium pt-6 pb-2'>Beautiful Designs</h3>
+              <p className='py-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.
+              </p>
+              <h4 className='py-4 text-teal-900'>Desgin tools I use</h4>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p className='text-gray-800 py-1'>Illustrator</p>
+              <p className='text-gray-800 py-1'>Figma</p>
+            </div>
+            <div className='text-center bg-gradient-to-r from-cyan-100 to-teal-200 shadow-lg p-10 rounded-xl my-10'>
+              <Image src={code} width={100} height={100} alt={''} className='mx-auto' />
+              <h3 className='text-lg font-medium pt-6 pb-2'>Beautiful Designs</h3>
+              <p className='py-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.
+              </p>
+              <h4 className='py-4 text-teal-900'>Desgin tools I use</h4>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p className='text-gray-800 py-1'>Illustrator</p>
+              <p className='text-gray-800 py-1'>Figma</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            
+          </div>
         </section>
       </main>
 
